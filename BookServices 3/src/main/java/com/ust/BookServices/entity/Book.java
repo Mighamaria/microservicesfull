766d7896837d4c0108b789dabbcc6a4f00a9e756
Mighamaria <mighka.maria2@gamil.com>
2023-05-18 23:59:18 +0530

    new3

---
+++ BookServices 3/src/main/java/com/ust/BookServices/entity/Book.java
@@ -0,0 +1,14 @@
+package com.ust.BookServices.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private Integer bookId;
+    private String bookName;
+    private double bookCost;
+}
